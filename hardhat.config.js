eae8060730f2a10caf7ffd33fdea3df812c3037f
vishnu <vappu5429@gmail.com>
2023-01-01 20:33:54 +0530

    updated Raffle

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,35 @@
-require("@nomiclabs/hardhat-waffle")
-require("@nomiclabs/hardhat-etherscan")
-require("hardhat-deploy")
-require("solidity-coverage")
-require("hardhat-gas-reporter")
-require("hardhat-contract-sizer")
-require('dotenv').config()
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const MAINNET_RPC_URL = process.env.ALCHEMY_MAINNET_RPC_URL || " https://eth-mainnet.g.alchemy.com/v2/SsniJWcZxvPEI8tL2M303HKGLjTPRMV3"
-
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli.g.alchemy.com/v2/kRq1RDRlZeyHIYwIzxSjGNzqvQXLhwSK"
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "b64f9cf420d9e315a46d82e204b5672e532d30ca1ca645a6346dc2cde93f42da"
+const MAINNET_RPC_URL =
+  process.env.MAINNET_RPC_URL ||
+  process.env.ALCHEMY_MAINNET_RPC_URL ||
+  "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL ||
+  "https://eth-goerli.alchemyapi.io/v2/your-api-key";
+const POLYGON_MAINNET_RPC_URL =
+  process.env.POLYGON_MAINNET_RPC_URL ||
+  "https://polygon-mainnet.alchemyapi.io/v2/your-api-key";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
 // optional
+const MNEMONIC = process.env.MNEMONIC || "your mnemonic";
 
 // Your API key for Etherscan, obtain one at https://etherscan.io/
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "RW3U2A64IE9DX324NPWZGQI92WCAS1E6TB"
-const REPORT_GAS = process.env.REPORT_GAS || true
+const ETHERSCAN_API_KEY =
+  process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
+const POLYGONSCAN_API_KEY =
+  process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key";
+const REPORT_GAS = process.env.REPORT_GAS || false;
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -51,12 +62,18 @@ module.exports = {
       saveDeployments: true,
       chainId: 1,
     },
-
+    polygon: {
+      url: POLYGON_MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 137,
+    },
   },
   etherscan: {
     // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
     apiKey: {
       goerli: ETHERSCAN_API_KEY,
+      polygon: POLYGONSCAN_API_KEY,
     },
     customChains: [
       {
@@ -71,7 +88,7 @@ module.exports = {
   },
   gasReporter: {
     enabled: REPORT_GAS,
-    currency: 'CHF',
+    currency: "USD",
     outputFile: "gas-report.txt",
     noColors: true,
     // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
@@ -102,4 +119,4 @@ module.exports = {
   mocha: {
     timeout: 500000, // 500 seconds max for running tests
   },
-}
+};
